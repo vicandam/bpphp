cd9086baa553a463db4a8e0e28f5999db6d930bb
vicrealtime <vic.andam@realtimemarketing.com>
2025-07-21 23:05:59 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -168,25 +168,4 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::post('/users/{user}/elevate-golden-hearts-awardee', [UserController::class, 'elevateToGoldenHeartsAwardee'])->name('users.elevate_golden_hearts_awardee');
 });
 
-//Route::middleware(['auth', 'verified'])->group(function () {
-//    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//
-//    Route::put('/ghl-settings', [GHLSettingsController::class, 'update'])->name('ghl.settings.update');
-//
-//    Route::get('/contacts/{id}/edit', [GHLContactController::class, 'edit'])->name('contacts.edit');
-//    Route::put('/contacts/{id}', [GHLContactController::class, 'update'])->name('contacts.update');
-//    Route::delete('/contacts/{id}', [GHLContactController::class, 'destroy'])->name('contacts.destroy');
-//
-//    Route::get('/contacts/create', [DashboardController::class, 'create'])->name('contacts.create');
-//    Route::post('/contacts', [GHLContactController::class, 'store'])->name('contacts.store');
-//
-//    Route::get('/account', [DashboardController::class, 'account'])->name('account');
-
-//    Route::put('/user/password', [CustomPasswordController::class, 'update'])->middleware(['auth'])->name('password.update');
-//});
-
 require __DIR__.'/auth.php';
