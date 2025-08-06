522c193fbc19580f74aeb6b8d6f316f7f1283a10
Vic Andam <vicajobs@gmail.com>
2025-08-06 23:27:37 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,13 +18,19 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <title>
-    BPPHP Movement
-  </title>
+
+    <title>
+        Login To BPPHP Movement
+    </title>
+
+    <link rel="icon" type="image/png" href="https://assets.cdn.filesafe.space/rXM1uS2u3mzz5DBmQq5L/media/6880e0be12dcea2d369279b7.png">
+    <link rel="apple-touch-icon" href="https://assets.cdn.filesafe.space/rXM1uS2u3mzz5DBmQq5L/media/6880e0be12dcea2d369279b7.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://assets.cdn.filesafe.space/rXM1uS2u3mzz5DBmQq5L/media/6880e0be12dcea2d369279b7.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://assets.cdn.filesafe.space/rXM1uS2u3mzz5DBmQq5L/media/6880e0be12dcea2d369279b7.png">
+
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+
   <!-- Nucleo Icons -->
     <link href="{{ asset('themes/material-dashboard/assets/css/nucleo-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('themes/material-dashboard/assets/css/nucleo-svg.css') }}" rel="stylesheet">
